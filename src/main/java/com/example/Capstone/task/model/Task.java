0a09bee5742368caffcb66e3c1cb7d0208bcb8de
Mason Waltke <mqwcgc@umsystem.edu>
2025-03-28 12:20:23 -0500

    Added Issac's additions to task entity

--- src/main/java/com/example/Capstone/task/model/Task.java
+++ src/main/java/com/example/Capstone/task/model/Task.java
@@ -17,9 +17,62 @@ public class Task {
     @GeneratedUuidV7
     private UUID id;
 
+    @Column(name = "status", nullable = false)
+    private String status;
+
     @Column(name = "name")
     private String name;
 
     @Column(name = "description")
     private String description;
+
+    @Column(name = "time_spent")
+    private Integer timeSpent;
+
+    @Column(name = "time_intended")
+    private Integer timeIntended;
+
+    @Column(name = "started_work")
+    private Long startedWork; // Unix timestamp in seconds
+
+    @Column(name = "ceased_work")
+    private Long ceasedWork; // Unix timestamp in seconds
+
+    @Column(name = "parent_task")
+    private UUID parentTask;
+
+    @Column(name = "child_task")
+    private String childTask;
+
+    @Column(name = "category")
+    private String category;
+
+    @Column(name = "scheduled_start")
+    private Long scheduledStart; // Unix timestamp in seconds
+
+    @Column(name = "scheduled_end")
+    private Long scheduledEnd; // Unix timestamp in seconds
+
+    @Column(name = "num_breaks")
+    private Integer numBreaks;
+
+    @Column(name = "longest_work_stretch")
+    private Integer longestWorkStretch; // Assuming this is in seconds
+
+    @Column(name = "time_log")
+    private UUID timeLog;
+
+    @Column(name = "in_focus_list")
+    private Boolean inFocusList;
+
+    @Column(name = "selected")
+    private Boolean selected;
+
+    @Column(name = "intent_list")
+    private String intentList;
+
+    @Column(name = "custom_param")
+    private String customParam;
+
+
 }
--- src/main/java/com/example/Capstone/task/model/TaskDTO.java
+++ src/main/java/com/example/Capstone/task/model/TaskDTO.java
@@ -7,12 +7,47 @@ import java.util.UUID;
 @Data
 public class TaskDTO {
     private UUID id;
+    private String status;
     private String name;
     private String description;
+    private Integer timeSpent;
+    private Integer timeIntended;
+    private Long startedWork; // Unix timestamp in seconds
+    private Long ceasedWork; // Unix timestamp in seconds
+    private UUID parentTask;
+    private String childTask;
+    private String category;
+    private Long scheduledStart; // Unix timestamp in seconds
+    private Long scheduledEnd; // Unix timestamp in seconds
+    private Integer numBreaks;
+    private Integer longestWorkStretch;
+    private UUID timeLog; // Not positive this is the right variable for this, you may need to check me
+    private Boolean inFocusList;
+    private Boolean selected;
+    private String intentList;
+    private String customParam;
 
     public TaskDTO(Task task) {
-        this.description = task.getDescription();
-        this.name = task.getName();
         this.id = task.getId();
+        this.status = task.getStatus();
+        this.name = task.getName();
+        this.description = task.getDescription();
+        this.timeSpent = task.getTimeSpent();
+        this.timeIntended = task.getTimeIntended();
+        this.startedWork = task.getStartedWork();
+        this.ceasedWork = task.getCeasedWork();
+        this.parentTask = task.getParentTask();
+        this.childTask = task.getChildTask();
+        this.category = task.getCategory();
+        this.scheduledStart = task.getScheduledStart();
+        this.scheduledEnd = task.getScheduledEnd();
+        this.numBreaks = task.getNumBreaks();
+        this.longestWorkStretch = task.getLongestWorkStretch();
+        this.timeLog = task.getTimeLog();
+        this.inFocusList = task.getInFocusList();
+        this.selected = task.getSelected();
+        this.intentList = task.getIntentList();
+        this.customParam = task.getCustomParam();
+
     }
 }
